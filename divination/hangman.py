e2f0ed46d62a6e71681c04880486402970c09978
Vinicius Serpa <vinicius.serpa@gmail.com>
2018-04-07 12:14:18 -0300

    hangman first version

--- divination/hangman.py
+++ divination/hangman.py
@@ -3,7 +3,28 @@ def play():
     print("**********Hangman Game!**********")
     print("*********************************")
 
+    secret_word = "banana"
+    total_tentatives = 6
+    tentative = 1
+    hanged = False
+    win = False
+
+    while (not(hanged) and not(win) and tentative <= total_tentatives):
+        print("playing...")
+
+        user_char = input("Type an character: ")
+
+        index = 0
+
+        for c in secret_word:
+            if (c == user_char):
+                print("Find char {} at position {}".format(c, index))
+            index = index + 1
+
+        tentative = tentative + 1
+
     print("Game over")
+
 
 if (__name__ == "__main__"):
     play()
